4c7e03100110a105a5c3a5f2d5978b11e2bc57eb
farhandardiri <faran.f4124n@gmail.com>
2021-04-01 21:17:42 +0700

    install vue router

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 // import Ninjas from './components/Ninjas.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 
 // Vue.component('ninjas',Ninjas);
 // export const bus = new Vue();
 // ---
 
 Vue.use(VueResource)
+Vue.use(VueRouter)
 
 // awal filter
 
