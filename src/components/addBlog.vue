80ed2f6c12449bdcb56fe645d6179d59d3da1713
farhandardiri <faran.f4124n@gmail.com>
2021-04-02 14:34:29 +0700

    edit file addBlog to posting firebase

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -61,11 +61,10 @@ export default {
   methods: {
     post: function () {
       this.$http
-        .post("https://jsonplaceholder.typicode.com/posts", {
-          title: this.blog.title,
-          body: this.blog.content,
-          userId: 1,
-        })
+        .post(
+          "https://blink-netninja-default-rtdb.firebaseio.com/posts.json",
+          this.blog
+        )
         .then(function (data) {
           console.log(data);
           this.submitted = true;
