a241012509a22db27f4622a47fdeb239fb24c27e
farhandardiri <faran.f4124n@gmail.com>
2021-04-02 12:37:21 +0700

    add file & router sigleBlog

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import showBlogs from './components/showBlog'
 import addBlog from './components/addBlog'
+import sigleBlog from './components/sigleBlog'
 
 export default [
     {path:'/', component:showBlogs},
     {path:'/add', component:addBlog},
+    {path:'blog/:id', component:sigleBlog}
 ]
